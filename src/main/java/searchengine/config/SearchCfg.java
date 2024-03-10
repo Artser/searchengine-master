ab106276efbee7a65ebae94a86f74a02f3e225a7
Artser <artserlisa@mail.ru>
2024-03-10 22:59:49 +0300

    1003

--- src/main/java/searchengine/config/SearchCfg.java
+++ src/main/java/searchengine/config/SearchCfg.java
@@ -1,16 +1,19 @@
 package searchengine.config;
 
+
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Getter
 @Setter
 @Component
-@ConfigurationProperties(prefix = "indexing-settings")
-public class SitesList {
-    private List<Site> sites;
+@ConfigurationProperties(prefix = "search-settings")
+public class SearchCfg {
+    private String query;
+    private String site;
+    private int offset;
+    private int limit;
+
 }
---
+++ src/main/java/searchengine/model/EntityPage.java
@@ -0,0 +1,32 @@
+package searchengine.model;
+
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "page")
+public class EntityPage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Integer id;
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
+    private EntitySite site;
+
+    @Column(columnDefinition = "TEXT NOT NULL, UNIQUE KEY uk_site_path(path(250),site_id)")
+    private String path;
+    @Column(nullable = false)
+    private Integer code;
+    //@Column(columnDefinition = "MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci", nullable = false)
+    @Column(columnDefinition = "MEDIUMTEXT SET utf8mb4 COLLATE utf8mb4_general_ci", nullable = false)
+    private String content;
+
+}
